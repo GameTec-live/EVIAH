37295aba1d058b83a3d20d333b1bc79ec12910db
GameTec_live <benedikt.werner08@gmail.com>
2022-12-02 17:31:49 +0100

    added another dependency

--- eviah.sh
+++ eviah.sh
@@ -6,7 +6,7 @@ EVIAH_SRCDIR="$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
 for script in "${EVIAH_SRCDIR}/LIBS/"*.sh; do . "${script}"; done
 set_globals
 
-deps=("git" "curl" "wget" "python3" "python3-pip" "python3-venv" "zfsutils-linux")
+deps=("git" "curl" "wget" "python3" "python3-pip" "python3-venv" "zfsutils-linux" "software-properties-common")
 dependency_check "${deps[@]}"
 clear
 echo "${green}Dependencies installed! ${white}"
